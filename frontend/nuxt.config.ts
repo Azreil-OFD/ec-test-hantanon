56f1eb4cc8bb459f79428e0459c167e6e9f2dfcc
Azreil-OFD <fantom2413@gmail.com>
2024-11-20 07:17:36 +0500

    [frontend] add registration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,28 @@
+import Aura from '@primevue/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   mode: 'static',
+  modules: [
+   '@primevue/nuxt-module'
+   ],
+   css: ['~/assets/css/main.css'],
   router: {
       base: '/ec-test-hantanon/'
    },
+   primevue: {
+      options: {
+          theme: {
+              preset: Aura
+          }
+      }
+  },
+  postcss: {
+   plugins: {
+     tailwindcss: {},
+     autoprefixer: {},
+   },
+ },
+
 })
